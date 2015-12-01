fe90b7918e355d8652c0c7be704b0a52f743d234
BetkaB <a.bachronikova@gmail.com>
2015-12-01 08:49:00 +0100

    first layout

--- app/src/main/java/dk/cngroup/m_calendar/MainActivity.java
+++ app/src/main/java/dk/cngroup/m_calendar/MainActivity.java
@@ -1,11 +1,30 @@
 package dk.cngroup.m_calendar;
 
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
 
 @EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
+
+    @ViewById(R.id.organizedBy)
+    TextView organizedBy;
+
+    @ViewById(R.id.status)
+    TextView status;
+
+    @AfterViews
+    public void init(){
+        String organizator = "Abc";
+        String text = getBaseContext().getResources().getString(R.string.organizedBy, organizator);
+        organizedBy.setText(text);
+
+    }
+
 
 
 }
